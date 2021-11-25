611d0a90fbd0ca3d06d751ce347c819e5425c60c
crazyguevaraa <70381198+crazyguevaraa@users.noreply.github.com>
2021-11-25 08:45:42 -0800

    Add files via upload

--- prob_fifo.c
+++ prob_fifo.c
@@ -10,7 +10,8 @@
 
 int main (int argc, char* argv[]){
 
-	int size;
+	int size = 0;
+	int status = 0;
 
 	int fd_global_fifo = 0;
 	int fd_local_fifo  = 0;
@@ -41,12 +42,86 @@ int main (int argc, char* argv[]){
 
 	if (argc > 1 && argc < 3){
 
-			
+		int file_status = 0;
+
+		if ((file_status = open (argv[1], O_RDONLY)) == -1){
+				
+			printf ("Cannot open FILE\n");
+			exit (1);
+		}	
+
 		//
 		//connect to global FIFO
 		//
 
+		if ((fd_global_fifo = open (global_name, O_RDONLY)) == -1){
+			
+			printf ("Cannot connect to group\n");
+			exit (1);
+		}
+		//
+		//read a local name in global FIFO
+		//
+		
+		if ((read (fd_global_fifo, uniq_name, uniq_name_size)) != uniq_name_size){
+
+			printf ("Cannot reading uniq name\n");
+			exit (1);
+		}
+		//
+		//connect to local fifo
+		//
+
+		if ((fd_local_fifo = open (uniq_name, O_WRONLY | O_NONBLOCK)) == -1){
+			
+			printf ("Cannot connect to local FIFO\n");
+			exit (1);
+		}
+
+		close (fd_global_fifo);
+
+		if ((status = fcntl (fd_local_fifo, F_SETFL, O_WRONLY)) == -1){
+			
+			printf ("Cannot update local fifo\n");
+			exit (1);
+		}
+
+		//
+		//copy file to local FIFO
+		//
+		
+		for (;;){
+			
+			if ((size = read (file_status, buffer, buffer_size)) && (write (fd_local_fifo, buffer, size)))
+				continue;
+		
+			else {
+				close (file_status);
+				break;
+		
+			}
+		}
+			
+		unlink (uniq_name);
+		close (file_status);
+	}
+		
+	/////////////////////////////////////////////
+	// part of programm, that reads to FIFO
+	/////////////////////////////////////////////
+
+	else {
+		int fd_global_fifo_2;
+		///
+		//connect to global FIFO
+		//
+
 		if ((fd_global_fifo = open (global_name, O_WRONLY)) == -1){
+			
+			printf ("Cannot connect to group\n");
+			exit (1);
+		}
+		if ((fd_global_fifo_2 = open (global_name, O_WRONLY)) == -1){
 			
 			printf ("Cannot connect to group\n");
 			exit (1);
@@ -61,97 +136,7 @@ int main (int argc, char* argv[]){
 		int local_name_status = sprintf (uniq_name, "local%d", pid);
 
 		assert (local_name_status);
-	
-		//
-		//write a local name in global FIFO
-		//
-		
-		if ((write (fd_global_fifo, uniq_name, uniq_name_size))){
 
-			printf ("Cannot writing uniq name\n");
-			exit (1);
-		}
-		
-		else printf("uniq name is OK\n");	
-		/*//
-		//create local FIFO
-		//
-
-		int local_status = mknod (uniq_name, S_IFIFO | 0666, 0);
-		if (local_status < 0){
-			
-			printf ("Cannot create local FIFO\n");
-			exit (1);
-		}*/
-		sleep(10);
-
-//
-//тут при смерти записывающего процесса процесса
-//читающий на веки вечные становится в ожидании в open()
-//
-
-		//
-		//connect to local fifo
-		//
-
-		if ((fd_local_fifo = open (uniq_name, O_WRONLY)) == -1){
-			
-			printf ("Cannot connect to local FIFO\n");
-			exit (1);
-		}
-
-		//
-		//copy file to local FIFO
-		//
-
-		
-		int file_status = 0;
-
-		if ((file_status = open (argv[1], O_RDONLY)) == -1){
-				
-			printf ("Cannot open FILE\n");
-			exit (1);
-		}	
-		
-		for (;;){
-			
-			if ((size = read (file_status, buffer, buffer_size)) && (write (fd_local_fifo, buffer, size)))
-				continue;
-		
-			else {
-				close (file_status);
-				break;
-		
-			}
-		}
-
-		}		
-	/////////////////////////////////////////////
-	// part of programm, that reads to FIFO
-	/////////////////////////////////////////////
-
-	else {
-		
-		///
-		//connect to global FIFO
-		//
-
-		if ((fd_global_fifo = open (global_name, O_RDONLY)) == -1){
-			
-			printf ("Cannot connect to group\n");
-			exit (1);
-		}
-
- 		//
-		//read a local name in global FIFO
-		//
-		
-		if ((read (fd_global_fifo, uniq_name, uniq_name_size)) < 0){
-
-			printf ("Cannot reading uniq name\n");
-			exit (1);
-		}
-		
 		//
 		//create local FIFO
 		//
@@ -163,7 +148,6 @@ int main (int argc, char* argv[]){
 			exit (1);
 		}
 
-		
 		//
 		//connect to local fifo
 		//
@@ -173,6 +157,28 @@ int main (int argc, char* argv[]){
 			printf ("Cannot connect to local FIFO\n");
 			exit (1);
 		}
+		
+		//
+		//write a local name in global FIFO
+		//
+		
+		if ((write (fd_global_fifo, uniq_name, uniq_name_size)) <= 0){
+
+			printf ("Cannot writing uniq name\n");
+			exit (1);
+		}
+		
+		else printf("uniq name is OK\n");
+		
+		close (fd_global_fifo);
+
+		if ((status = fcntl (fd_local_fifo, F_SETFL, O_RDONLY)) == -1){
+		
+			printf ("Cannot update local fifo (read)\n");
+			exit (1);
+		}
+		
+		sleep (10);
 
 		//
 		//reading a file from local FIFO
@@ -192,7 +198,7 @@ int main (int argc, char* argv[]){
 	}
 
         unlink (uniq_name);
-	unlink (global_name);
+	//unlink (global_name);
 }
 
 
--- prob_msg.c
+++ prob_msg.c
@@ -9,10 +9,12 @@
 
 
 struct my_message {
+
     long type;
 };
 
 int main (int argc, char* argv[]){
+
     //scanning num of chilrens
     char* endptr = NULL;
 
@@ -22,9 +24,10 @@ int main (int argc, char* argv[]){
 
     if (*endptr == 0 && num_of_child > 0){
 
-        int msq_id = msgget (IPC_PRIVATE, O_CREAT | 0666);
+        int msq_id = msgget (IPC_PRIVATE, IPC_CREAT | 0666);
 
         if (msq_id < 0){
+
             printf ("Cannot create msq! msgget error\n");
             exit(1);
         }
@@ -34,27 +37,33 @@ int main (int argc, char* argv[]){
         pid_t pid = 0;
 
         for (long number = 1; number <= num_of_child; number++){
+
             pid = fork ();
 
-            if (pid = -1){
+            if (pid == -1){
+
                 printf ("Cannot born child\n");
                 exit(1);
             }
     //in child process
-            else if (pid = 0){
+            else if (pid == 0){
 
                 struct my_message msg;
+
     //recive messefe from daddy
                 if (msgrcv (msq_id, &msg, sizeof (msg) - sizeof (long), number, MSG_NOERROR) == -1){
+
                     printf ("cannot recive message from daddy, child #%ld\n", number);
                     exit(1);
                 } 
+
                 printf("I`m child #%ld\n", number);
                 //fflush(0);
     //send message to daddy
                 msg.type = num_of_child + 1;
 
                 if (msgsnd (msq_id, &msg, sizeof (msg) - sizeof (long), MSG_NOERROR) == -1){
+
                     printf ("cannot send message for daddy, child #%ld\n", number);
                     exit(1);
                 }
@@ -70,24 +79,24 @@ int main (int argc, char* argv[]){
             msg.type = number;
     //send message to child #number
             if (msgsnd (msq_id, &msg, sizeof (msg) - sizeof (long), MSG_NOERROR) == -1){
+
                 printf ("cannot send message to child #%ld\n", number);
                 exit (1);
             }
     //recive message from child #number
             if (msgrcv (msq_id, &msg, sizeof (msg) - sizeof (long), num_of_child + 1, MSG_NOERROR) == -1){
+
                 printf ("cannot recive message from child #%ld\n", number);
                 exit (1);
             }
     //check correctly reciving messages from childrens
             if (msgctl (msq_id, IPC_RMID, NULL) != 0){
+
                 printf ("we have a problem, msgctl () error\n");
                 exit (1);
             }
-
-        }
-        else printf ("oh shit , I`m sorry\n");
-        return 0;
-
+            //else printf ("oh shit , I`m sorry\n");
+	}
     }
 
 }
